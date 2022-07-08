b3754b494fd525d5576a7170981d62b1cc1a693a
Temerold <tobbe@temerold.se>
2022-07-08 12:01:47 +0200

    Add logging

--- src/index.js
+++ src/index.js
@@ -1,44 +1,93 @@
 const Discord = require("discord.js");
+const { PassThrough } = require("stream");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const { TOKEN } = require("../config.json");
+const { DEBUG, TOKEN } = require("../config.json");
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("messageCreate", (msg) => {
-    if (msg.author.id === client.user.id) return;
-    var reply = [];
+    // If there are no "ing"'s in `msg.content`, or if the message is sent by the bot,
+    // return
+    if (
+        !msg.content.toLowerCase().includes("ing") ||
+        msg.author.id == client.user.id
+    )
+        return;
 
-    // Replace all newlines in `msg` with a space
+    not_sent = false;
+    var reply_msg = createReply(msg);
+    msg.reply(reply_msg, (err) => {
+        if (err) {
+            console.error(err);
+            not_sent = true;
+        }
+    });
+
+    if (DEBUG["CONSOLE"] || DEBUG["FILE"]) {
+        if (DEBUG["CONSOLE"])
+            console.log(`Replied to ${msg.author.tag}; "${reply_msg}"`);
+        if (DEBUG["FILE"]) log_file(msg, reply_msg);
+    }
+});
+
+function createReply(msg) {
+    var reply_msg = [];
+
+    // Replace all newlines in `msg.content` with a space
     msg.content = msg.content.replaceAll("\n", " ");
 
-    // Replace all longer-than-one spaces in `msg` with a single space
+    // Replace all longer-than-one spaces in `msg.content` with a single space
     while (msg.content.includes("  ")) {
         msg.content = msg.content.replaceAll("  ", " ");
     }
 
-    var msg_array = msg.content.split(" "); // Array containing all words in `msg`
+    var msg_array = msg.content.split(" "); // Array containing all words in "ing"
 
-    // Go through `msg_array`, and for every word ending in "ing", add the word to `reply`
-    // except ending in "ong"
+    // Go through `msg_array`, and for every word ending in "ing", add the word to
+    // `reply_msg` except ending in "ong"
     msg_array.forEach((word) => {
         word_beginning = word.slice(0, -3);
         word_ending = word.slice(-3);
 
-        if (word_ending == "ing") {
-            reply.push(word_beginning + "ong");
-        }
+        if (word_ending == "ing") reply_msg.push(word_beginning + "ong");
     });
 
-    // If `reply` is empty, return
-    if (JSON.stringify(reply) === JSON.stringify([])) return;
+    // Make the `reply_msg` array a string, the words seperated by ", ". Also make the
+    // first letter capital, and add a dot at the end.
+    var reply_msg = reply_msg.join(", ");
+    var reply_msg = reply_msg[0].toUpperCase() + reply_msg.slice(1) + ".";
+    return reply_msg;
+}
 
-    // Make the `reply` array a string, the words seperated by ", ". Also make the first
-    // letter capital, and add a dot at the end.
-    var reply = reply.join(", ");
-    var reply = reply[0].toUpperCase() + reply.slice(1) + ".";
+function log_file(msg, reply_msg, not_sent) {
+    // This script's and the `logs` folder's paths
+    var script_path = __filename.substring(0, __filename.lastIndexOf("\\") + 1);
+    var logs_path = script_path + "logs\\log.json";
 
-    msg.reply(reply);
-});
+    const fs = require("fs");
+    try {
+        var logs = require(logs_path);
+    } catch (err) {
+        // The "SyntaxError: Unexpected end of JSON input" error gets thrown when the log
+        // file is empty. If it is, add "{}" to it.
+        if (err instanceof SyntaxError) {
+            fs.writeFileSync(logs_path, "{}", (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            });
+
+            var logs = require(logs_path);
+        }
+    }
+    if (not_sent) logs["notSent"] = true;
+    logs[msg.id] = msg;
+    logs[msg.id]["replyContent"] = reply_msg;
+    fs.writeFile(logs_path, JSON.stringify(logs), (err) => {
+        if (err) console.error(err);
+    });
+}
 
 client.login(TOKEN);
