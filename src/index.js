c4589f27bddc11db1379098f4b79e1c483431572
Temerold <tobbe@temerold.se>
2022-07-09 22:51:12 +0200

    Add debugging options and message punctuation ignoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
-const { PassThrough } = require("stream");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const { DEBUG, TOKEN } = require("../config.json");
+const { DEBUG, SPECIAL_CHARS, TOKEN } = require("../config.json");
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
@@ -15,25 +14,25 @@ client.on("messageCreate", (msg) => {
     )
         return;
 
-    not_sent = false;
+    var sent = true;
     var reply_msg = createReply(msg);
-    msg.reply(reply_msg, (err) => {
-        if (err) {
-            console.error(err);
-            not_sent = true;
-        }
-    });
+    if (reply_msg != false) {
+        msg.reply(reply_msg, (err) => {
+            if (err) {
+                console.error(err);
+                sent = false;
+            }
+        });
 
-    if (DEBUG["CONSOLE"] || DEBUG["FILE"]) {
-        if (DEBUG["CONSOLE"])
-            console.log(`Replied to ${msg.author.tag}; "${reply_msg}"`);
-        if (DEBUG["FILE"]) log_file(msg, reply_msg);
+        if (DEBUG["CONSOLE"] || DEBUG["FILE"]) {
+            if (DEBUG["CONSOLE"])
+                console.log(`Replied to ${msg.author.tag}; "${reply_msg}"`);
+            if (DEBUG["FILE"]) log_file(msg, reply_msg, sent);
+        }
     }
 });
 
 function createReply(msg) {
-    var reply_msg = [];
-
     // Replace all newlines in `msg.content` with a space
     msg.content = msg.content.replaceAll("\n", " ");
 
@@ -42,10 +41,15 @@ function createReply(msg) {
         msg.content = msg.content.replaceAll("  ", " ");
     }
 
-    var msg_array = msg.content.split(" "); // Array containing all words in "ing"
+    // Remove all special characters in `msg.content`
+    var char_array = msg.content.split("");
+    char_array = char_array.filter((char) => !SPECIAL_CHARS.includes(char));
+    msg.content = char_array.join("");
+    var msg_array = msg.content.split(" "); // Array containing all words in the message
 
     // Go through `msg_array`, and for every word ending in "ing", add the word to
     // `reply_msg` except ending in "ong"
+    var reply_msg = [];
     msg_array.forEach((word) => {
         word_beginning = word.slice(0, -3);
         word_ending = word.slice(-3);
@@ -53,14 +57,19 @@ function createReply(msg) {
         if (word_ending == "ing") reply_msg.push(word_beginning + "ong");
     });
 
+    // If the reply is empty, it means that the message isn't a valid "ing"-ending
+    // message, and therefore no reply should be sent
+    if (reply_msg == []) return false;
+
     // Make the `reply_msg` array a string, the words seperated by ", ". Also make the
     // first letter capital, and add a dot at the end.
-    var reply_msg = reply_msg.join(", ");
-    var reply_msg = reply_msg[0].toUpperCase() + reply_msg.slice(1) + ".";
+    reply_msg = reply_msg.join(", ");
+    reply_msg = reply_msg[0].toUpperCase() + reply_msg.slice(1) + ".";
+
     return reply_msg;
 }
 
-function log_file(msg, reply_msg, not_sent) {
+function log_file(msg, reply_msg, sent) {
     // This script's and the `logs` folder's paths
     var script_path = __filename.substring(0, __filename.lastIndexOf("\\") + 1);
     var logs_path = script_path + "logs\\log.json";
@@ -82,7 +91,7 @@ function log_file(msg, reply_msg, not_sent) {
             var logs = require(logs_path);
         }
     }
-    if (not_sent) logs["notSent"] = true;
+    if (!sent) logs["sent"] = false;
     logs[msg.id] = msg;
     logs[msg.id]["replyContent"] = reply_msg;
     fs.writeFile(logs_path, JSON.stringify(logs), (err) => {
