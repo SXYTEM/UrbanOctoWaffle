03a118411b2c04c19f240bac7bd276980ec8e94d
Temerold <tobbe@temerold.se>
2022-07-10 22:14:51 +0200

    Add numbering of equally-named log files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const { TOKEN, SPECIAL_CHARS, DEBUG } = require("../config.json");
-const TIME = get_current_time();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const LOG_PATH = create_log_file();
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
@@ -17,9 +17,8 @@ client.on("messageCreate", (msg) => {
     ) {
         return;
     }
-
     var sent = true;
-    var reply_msg = createReply(msg);
+    var reply_msg = create_reply(msg);
     if (reply_msg != false) {
         msg.reply(reply_msg, (err) => {
             if (err) {
@@ -36,20 +35,47 @@ client.on("messageCreate", (msg) => {
     }
 });
 
-function get_current_time() {
-    // Get current time, and convert it to a string formatted like so: "YYYYMMDDhhmm"
-    var time = new Date();
-    var YYYY = time.getFullYear();
-    var MM = (time.getMonth() + 1).toString().padStart(2, "0");
-    var DD = time.getDay().toString().padStart(2, "0");
-    var hh = time.getHours();
-    var mm = time.getMinutes();
-    var TIME = `${YYYY}${MM}${DD}${hh}${mm}`;
+client.login(TOKEN);
 
-    return TIME;
+function create_log_file() {
+    // The `msg_logs` folder's paths
+    var script_path = __filename.substring(0, __filename.lastIndexOf("\\") + 1);
+    var log_folder = script_path + "msg_logs";
+
+    const TIME = get_current_time();
+    var log_path = `${log_folder}\\${TIME}.json`;
+
+    // If the `msg_logs` folder doesn't exist, create it
+    if (!fs.existsSync(log_folder)) {
+        fs.mkdirSync(log_folder, { recursive: true });
+    }
+
+    if (!fs.existsSync(log_path)) {
+        fs.open(log_path, "w", (err) => {
+            if (err) console.error(err);
+        });
+    } else {
+        // If a log file already exists for this exact date and time (to the minute),
+        // create a new file, except ending in a number. Starting at 1, the number goes up
+        // for every name duplicate. Formatted like so, where X is `duplicate_number`:
+        // "YYYYMMDDhhmm_X"
+        var duplicate_number = 1;
+        while (true) {
+            var path = `${log_folder}\\${TIME}_${duplicate_number}.json`;
+            if (!fs.existsSync(path)) {
+                fs.open(path, "w", (err) => {
+                    if (err) console.error(err);
+                });
+                log_path = path;
+                break;
+            }
+            duplicate_number += 1;
+        }
+    }
+
+    return log_path;
 }
-
-function createReply(msg) {
+function create_reply(msg) {
     // Replace all newlines in `msg.content` with a space
     msg.content = msg.content.replaceAll("\n", " ");
 
@@ -76,60 +102,46 @@ function createReply(msg) {
 
     // If the reply is empty, it means that the message isn't a valid "ing"-ending
     // message, and therefore no reply should be sent
-    if (reply_msg == []) return false;
+    if (reply_msg.length === 0) return false;
+    else {
+        // Make the `reply_msg` array a string, the words seperated by ", ". Also make the
+        // first letter capital, and add a dot at the end.
+        reply_msg = reply_msg.join(", ");
+        reply_msg = reply_msg[0].toUpperCase() + reply_msg.slice(1) + ".";
 
-    // Make the `reply_msg` array a string, the words seperated by ", ". Also make the
-    // first letter capital, and add a dot at the end.
-    reply_msg = reply_msg.join(", ");
-    reply_msg = reply_msg[0].toUpperCase() + reply_msg.slice(1) + ".";
-
-    return reply_msg;
+        return reply_msg;
+    }
 }
 
 function log_file(msg, reply_msg, sent) {
-    // The `msg_logs` folder's paths
-    var script_path = __filename.substring(0, __filename.lastIndexOf("\\") + 1);
-    var log_folder = script_path + "msg_logs";
-
-    var log_path = `${log_folder}\\${TIME}.json`;
-
-    // If the `msg_logs` folder doesn't exist, create it
-    if (!fs.existsSync(log_folder)) {
-        fs.mkdirSync(log_folder, { recursive: true });
-    }
-
-    if (!fs.existsSync(log_path)) {
-        fs.open(log_path, "w", (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        });
-    }
-
     try {
-        var logs = require(log_path);
+        var logs = require(LOG_PATH);
     } catch (err) {
         // The "SyntaxError: Unexpected end of JSON input" error gets thrown when the
         // log file is empty. If it is, add "{}" to it.
-        fs.writeFileSync(log_path, "{}", (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
+        fs.writeFileSync(LOG_PATH, "{}", (err) => {
+            if (err) console.error(err);
         });
 
-        var logs = require(log_path);
+        var logs = require(LOG_PATH);
     }
     if (!sent) logs["sent"] = false;
     logs[msg.id] = msg;
     logs[msg.id]["replyContent"] = reply_msg;
-    fs.writeFileSync(log_path, JSON.stringify(logs), (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+    fs.writeFileSync(LOG_PATH, JSON.stringify(logs), (err) => {
+        if (err) console.error(err);
     });
 }
 
-client.login(TOKEN);
+function get_current_time() {
+    // Get current time, and convert it to a string formatted like so: "YYYYMMDDhhmm"
+    var time = new Date();
+    var YYYY = time.getFullYear();
+    var MM = (time.getMonth() + 1).toString().padStart(2, "0");
+    var DD = time.getDay().toString().padStart(2, "0");
+    var hh = time.getHours();
+    var mm = time.getMinutes();
+    var TIME = `${YYYY}${MM}${DD}${hh}${mm}`;
+
+    return TIME;
+}
